edf267be7dee9d0c95c572c324c26ae8fb5c0f03
Maxim <minytian@yandex.ru>
2019-06-11 00:13:33 +0300

    Исправил топ меню под телефон. Теперь смотрится всё гармонично. Так же добавил пару ссылок на свои прошлые сохранёнки в папку dist. Работают на картинке лого и пагинации(нумерация страниц)

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('browser-sync', function() {
 		notify: false,
 		// open: false,
 		// online: false, // Work Offline Without Internet Connection
-		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+		tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
 	})
 });
 
